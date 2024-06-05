eac4b7ce7e02056219a4e03e5a1a5cb4055adb14
Artur Bristot <arturbristot123@gmail.com>
2024-06-05 17:01:33 -0300

    update

---
+++ teleSus/src/screens/home.js
@@ -0,0 +1,19 @@
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native"; // Importe o hook
+
+export default function home() {
+  const navigation = useNavigation(); // Instancie o hook
+
+  return (
+    <>
+    <View>
+        <Text>Login</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Home")}
+      >
+        <Text>Home</Text>
+      </TouchableOpacity>
+      </View>
+    </>
+  );
+}
--- teleSus/src/screens/login.js
+++ teleSus/src/screens/login.js
@@ -1,14 +1,12 @@
 import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native"; // Importe o hook
 
-export default function App() {
+export default function login() {
   const navigation = useNavigation(); // Instancie o hook
 
   return (
     <>
-        <Text>Login</Text>
       <TouchableOpacity
-        style={styles.button}
         onPress={() => navigation.navigate("Home")}
       >
         <Text>Home</Text>
