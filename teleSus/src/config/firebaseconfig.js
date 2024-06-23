636a24d495c42df77bc571419d330d5d3ed0c549
arturbristot <128424203+arturbristot@users.noreply.github.com>
2024-06-22 22:45:36 -0300

    UPDATE

--- teleSus/src/config/firebaseconfig.js
+++ teleSus/src/config/firebaseconfig.js
@@ -1,19 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc, deleteDoc, addDoc } from "firebase/firestore";
-import {getAuth, onAuthStateChanged} from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  doc,
+  deleteDoc,
+  addDoc,
+} from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD5hmHXtKrUndGJlR4XeLjHc_3dX9z7ogk",
-    authDomain: "telesus-86fe3.firebaseapp.com",
-    projectId: "telesus-86fe3",
-    storageBucket: "telesus-86fe3.appspot.com",
-    messagingSenderId: "828089327175",
-    appId: "1:828089327175:web:35e720898e189826599b62"
-  };
+  apiKey: "AIzaSyD5hmHXtKrUndGJlR4XeLjHc_3dX9z7ogk",
+  authDomain: "telesus-86fe3.firebaseapp.com",
+  projectId: "telesus-86fe3",
+  storageBucket: "telesus-86fe3.appspot.com",
+  messagingSenderId: "828089327175",
+  appId: "1:828089327175:web:35e720898e189826599b62",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -21,4 +27,12 @@ const database = getFirestore(app);
 
 const auth = getAuth(app);
 
-export { database, collection, doc, deleteDoc, addDoc, auth, onAuthStateChanged};
+export {
+  database,
+  collection,
+  doc,
+  deleteDoc,
+  addDoc,
+  auth,
+  onAuthStateChanged,
+};
